d3bd086359121ec811582a61dd0a4de8a886d3db
Riley Snyder <rileysndr@gmail.com>
2021-01-30 23:05:20 -0600

    add second task to update activity

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # discord-stock-ticker
 
+live stock tickers for your discord server. due to discord limitations the prices in the usernames will be updated every hour.
+
 ## Add tickers to your servers
 
-[GME](https://discord.com/api/oauth2/authorize?client_id=805268557994262529&permissions=0&scope=bot)
+[gme](https://discord.com/api/oauth2/authorize?client_id=805268557994262529&permissions=0&scope=bot)
 
-[BB](https://discord.com/api/oauth2/authorize?client_id=805289769272999986&permissions=0&scope=bot)
+[bb](https://discord.com/api/oauth2/authorize?client_id=805289769272999986&permissions=0&scope=bot)
 
 [amc](https://discord.com/api/oauth2/authorize?client_id=805294017441038357&permissions=0&scope=bot)
 
--- main.py
+++ main.py
@@ -10,7 +10,8 @@ class Ticker(discord.Client):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.bg_task = self.loop.create_task(self.update_price())
+        self.sm_task = self.loop.create_task(self.update_name())
+        self.bg_task = self.loop.create_task(self.update_activity())
 
 
     async def on_ready(self):
@@ -18,7 +19,7 @@ class Ticker(discord.Client):
         print('Logged in as', self.user.name)
 
 
-    async def update_price(self):
+    async def update_name(self):
 
         await self.wait_until_ready()
 
@@ -27,22 +28,36 @@ class Ticker(discord.Client):
             print(f'Updating price for {getenv("TICKER")}')
 
             ticker = yf.Ticker(getenv('TICKER'))
+
             await self.user.edit(
                 username=f'{getenv("TICKER")} - ${ticker.info["regularMarketPrice"]}'
             )
 
-            diff = ticker.info["regularMarketPrice"] - ticker.info['open']
+            await asyncio.sleep(3605)
+    
+
+    async def update_activity(self):
+
+        await self.wait_until_ready()
+
+        while not self.is_closed():            
+            
+            print(f'Updating activity for {getenv("TICKER")}')
+
+            ticker = yf.Ticker(getenv('TICKER'))
+
+            diff = ticker.info['regularMarketPrice'] - ticker.info['open']
             if diff > 0:
                 diff = '+' + str(diff)
 
             await self.change_presence(
                 activity=discord.Activity(
                     type=discord.ActivityType.watching,
-                    name=diff
+                    name=f'{ticker.info["regularMarketPrice"]} - {diff}'
                 )
             )
 
-            await asyncio.sleep(3605)
+            await asyncio.sleep(60)
 
 
 client = Ticker()
