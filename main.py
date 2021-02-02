aedfcb2310ee4cbbfd93906f0e516a5951e28784
Riley Snyder <rileysndr@gmail.com>
2021-02-01 18:10:13 -0600

    Revert "remove time from log message, loki does it for me"
    This reverts commit 32da6ef82d81d2b1d8284cf7c2f40214ef8924f3.

--- main.py
+++ main.py
@@ -148,7 +148,8 @@ if __name__ == "__main__":
     logging.basicConfig(
         filename='discord-stock-ticker.log',
         level=logging.INFO,
-        format='%(levelname)-8s %(message)s'
+        datefmt='%Y-%m-%d %H:%M:%S',
+        format='%(asctime)s %(levelname)-8s %(message)s',
     )
 
     client = Ticker()
