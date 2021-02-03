5220cd0a4208c866e6950b57e4491cb4eb9c6ceb
Riley Snyder <rileysndr@gmail.com>
2021-02-02 18:50:43 -0600

    remove names from logs loki takes care of that

--- main.py
+++ main.py
@@ -43,7 +43,7 @@ class Ticker(discord.Client):
 
         # Connect to discord
         name = self.user.name.split(' ')
-        logging.info(f'{name[0]}: logged in')
+        logging.info('logged in')
 
         # We want to know where we are running
         servers = [x.name for x in list(self.guilds)]
@@ -59,32 +59,32 @@ class Ticker(discord.Client):
         old_price = ''
 
         await self.wait_until_ready()
-        logging.info(f'{ticker}: name ready')
+        logging.info('name ready')
 
         while not self.is_closed():
 
             start = time.time()
-            logging.info(f'{ticker}: name started')
+            logging.info('name started')
             
             # Grab the current price data
             data = yf.Ticker(ticker)
             price = data.info['bid']
-            logging.info(f'{ticker}: name price retrived {price}')
+            logging.info(f'name price retrived {price}')
 
             # Only update on price change
             if old_price != price:
                 await self.user.edit(
                     username=f'{ticker} - ${price}'
                 )
-                logging.info(f'{ticker}: name updated')
+                logging.info('name updated')
                 old_price = price
             else:
-                logging.info(f'{ticker}: no price change')
+                logging.info('no price change')
 
             # Only update every hour
             end = time.time()
             await asyncio.sleep(3600 - int(end - start))
-            logging.info(f'{ticker}: name sleep ended')
+            logging.info('name sleep ended')
     
 
     async def stock_update_activity(self):
@@ -96,12 +96,12 @@ class Ticker(discord.Client):
         old_price = ''
 
         await self.wait_until_ready()
-        logging.info(f'{ticker}: activity ready')
+        logging.info('activity ready')
 
         while not self.is_closed():
 
             start = time.time()
-            logging.info(f'{ticker}: activity started')
+            logging.info('activity started')
             
             # Grab the current price data w/ day difference
             data = yf.Ticker(ticker)
@@ -110,7 +110,7 @@ class Ticker(discord.Client):
             diff = round(diff, 2)
             if diff > 0:
                 diff = '+' + str(diff)
-            logging.info(f'{ticker}: activity price retrived {price}')
+            logging.info(f'activity price retrived {price}')
 
             # Only update on price change
             if old_price != price:
@@ -120,15 +120,15 @@ class Ticker(discord.Client):
                         name=f'${price} / {diff}'
                     )
                 )
-                logging.info(f'{ticker}: activity updated')
+                logging.info('activity updated')
                 old_price = price
             else:
-                logging.info(f'{ticker}: no price change')
+                logging.info('no price change')
 
             # Only update every min
             end = time.time()
             await asyncio.sleep(60 - int(end - start))
-            logging.info(f'{ticker}: activity sleep ended')
+            logging.info('activity sleep ended')
     
 
     async def crypto_update_name(self, gapi: CoinGeckoAPI):
@@ -141,32 +141,32 @@ class Ticker(discord.Client):
         old_price = ''
 
         await self.wait_until_ready()
-        logging.info(f'{name}: name ready')
+        logging.info('name ready')
 
         while not self.is_closed():
 
             start = time.time()
-            logging.info(f'{name}: name started')
+            logging.info('name started')
 
             # Grab the current price data
             data = gapi.get_price(ids=name, vs_currencies=CURRENCY)
             price = data.get(name, {}).get(CURRENCY)
-            logging.info(f'{name}: name price retrived {price}')
+            logging.info(f'name price retrived {price}')
 
             # Only update on price change
             if old_price != price:
                 await self.user.edit(
                     username=f'{ticker} - ${price}'
                 )
-                logging.info(f'{name}: name updated')
+                logging.info('name updated')
                 old_price = price
             else:
-                logging.info(f'{name}: no price change')
+                logging.info('no price change')
 
             # Only update every hour
             end = time.time()
             await asyncio.sleep(3600 - int(end - start))
-            logging.info(f'{name}: name sleep ended')
+            logging.info('name sleep ended')
     
 
     async def crypto_update_activity(self, gapi: CoinGeckoAPI):
@@ -178,17 +178,17 @@ class Ticker(discord.Client):
         old_price = ''
 
         await self.wait_until_ready()
-        logging.info(f'{name}: activity ready')
+        logging.info('activity ready')
 
         while not self.is_closed():
 
             start = time.time()
-            logging.info(f'{name}: activity started')       
+            logging.info('activity started')       
 
             # Grab the current price data
             data = gapi.get_price(ids=name, vs_currencies=CURRENCY)
             price = data.get(name, {}).get(CURRENCY)
-            logging.info(f'{name}: activity price retrived {price}')
+            logging.info(f'activity price retrived {price}')
 
             # Only update on price change
             if old_price != price:
@@ -198,15 +198,15 @@ class Ticker(discord.Client):
                         name=f'${price}'
                     )
                 )
-                logging.info(f'{name}: activity updated')
+                logging.info('activity updated')
                 old_price = price
             else:
-                logging.info(f'{name}: no price change')
+                logging.info('no price change')
 
             # Only update every min
             end = time.time()
             await asyncio.sleep(60 - int(end - start))
-            logging.info(f'{name}: activity sleep ended')
+            logging.info('activity sleep ended')
 
 if __name__ == "__main__":
 
