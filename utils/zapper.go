002bfa8ac0b552aa7671899dcf23128c8a25b9ea
Riley Snyder <rileysnyder@protonmail.ch>
2022-06-20 15:19:01 -0500

    fix: use zapper for gas data
    Co-authored-by: theding0x <theding0x@users.noreply.github.com>

--- utils/zapper.go
+++ utils/zapper.go
@@ -8,23 +8,78 @@ import (
 )
 
 const (
-	ZapperURL = "http://api.zapper.fi/v1/gas-price?network=%s&api_key=%s"
-	apiKey    = "96e0cc51-a62e-42ca-acee-910ea7d2a241"
+	ZapperURL = "https://api.zapper.fi/v2/gas-prices?network=%s&eip1559=%t"
 )
 
-func GetZapperData(network string) (GasData, error) {
+type ZapperEth1559Data struct {
+	Eip1559  bool `json:"eip1559"`
+	Standard struct {
+		BaseFeePerGas        int64 `json:"baseFeePerGas"`
+		MaxPriorityFeePerGas int64 `json:"maxPriorityFeePerGas"`
+		MaxFeePerGas         int64 `json:"maxFeePerGas"`
+	} `json:"standard"`
+	Fast struct {
+		BaseFeePerGas        int64 `json:"baseFeePerGas"`
+		MaxPriorityFeePerGas int64 `json:"maxPriorityFeePerGas"`
+		MaxFeePerGas         int64 `json:"maxFeePerGas"`
+	} `json:"fast"`
+	Instant struct {
+		BaseFeePerGas        int64 `json:"baseFeePerGas"`
+		MaxPriorityFeePerGas int64 `json:"maxPriorityFeePerGas"`
+		MaxFeePerGas         int64 `json:"maxFeePerGas"`
+	} `json:"instant"`
+}
 
-	var prices GasData
+type ZapperData struct {
+	Eip1559  bool  `json:"eip1559"`
+	Standard int64 `json:"standard"`
+	Fast     int64 `json:"fast"`
+	Instant  int64 `json:"instant"`
+}
 
-	reqUrl := fmt.Sprintf(ZapperURL, network, apiKey)
+func GetZapperData(network string, eip1559 bool) (ZapperData, error) {
+
+	var prices ZapperData
+
+	reqUrl := fmt.Sprintf(ZapperURL, network, eip1559)
 
 	req, err := http.NewRequest("GET", reqUrl, nil)
 	if err != nil {
 		return prices, err
 	}
-
-	req.Header.Add("User-Agent", "Mozilla/5.0")
 	req.Header.Add("accept", "application/json")
+	req.Header.Add("Authorization", "Basic OTZlMGNjNTEtYTYyZS00MmNhLWFjZWUtOTEwZWE3ZDJhMjQxOg==")
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		return prices, err
+	}
+
+	results, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return prices, err
+	}
+
+	err = json.Unmarshal(results, &prices)
+	if err != nil {
+		return prices, err
+	}
+
+	return prices, nil
+}
+
+func GetZapperEth1559() (ZapperEth1559Data, error) {
+
+	var prices ZapperEth1559Data
+
+	reqUrl := fmt.Sprintf(ZapperURL, "ethereum", true)
+
+	req, err := http.NewRequest("GET", reqUrl, nil)
+	if err != nil {
+		return prices, err
+	}
+	req.Header.Add("accept", "application/json")
+	req.Header.Add("Authorization", "Basic OTZlMGNjNTEtYTYyZS00MmNhLWFjZWUtOTEwZWE3ZDJhMjQxOg==")
 	client := &http.Client{}
 	resp, err := client.Do(req)
 	if err != nil {
